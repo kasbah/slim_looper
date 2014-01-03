24ae751f623378beb4da6c6550af3fa57d4a6df0
kasbah <kaspar.emanuel@gmail.com>
2014-01-03 23:56:49 +0100

    writing tests

--- test/test_looper.c
+++ test/test_looper.c
@@ -13,68 +13,155 @@ float out_buf[N_FRAMES];
 
 static Looper* setup_looper(void)
 {
-    Looper* looper = looper_new(N_FRAMES);
-    looper_reset(looper);
-    looper->input  = in_buf;
-    looper->output = out_buf;
-    return looper;
+    Looper* instance = looper_new(N_FRAMES);
+    looper_reset(instance);
+    instance->input  = in_buf;
+    instance->output = out_buf;
+    return instance;
 }
 
 //Test default silence while and while not recording.
 static char* test_record(void)
 {
 
-    Looper* looper = setup_looper();
-    float test_buf[N_FRAMES];
+    Looper* instance = setup_looper();
+    float test_var;
     for (int i = 0; i < N_FRAMES; i++)
     {
         in_buf[i]  = 0.1;
         out_buf[i] = 0.1;
-        test_buf[i]= 0.1;
     }
 
-    looper->settings->requested_state = SlimMessage_Looper_State_RECORD;
-    looper_run(looper, N_FRAMES);
+    instance->settings->requested_state = SlimMessage_Looper_State_RECORD;
+    looper_run(instance, N_FRAMES);
 
+    test_var = 0.1;
     for (int i = 0; i < N_FRAMES; i++)
     {
         mu_assert("test_record: not silent while recording", out_buf[i] == 0.0);
-        mu_assert("test_record: changed input while recording", in_buf[i] == test_buf[i]);
+        mu_assert("test_record: changed input while recording", in_buf[i] == test_var);
     }
 
-    looper->settings->requested_state = SlimMessage_Looper_State_PAUSE;
-    looper_run(looper, N_FRAMES);
+    instance->settings->requested_state = SlimMessage_Looper_State_PAUSE;
+    looper_run(instance, N_FRAMES);
 
     for (int i = 0; i < N_FRAMES; i++)
     {
         mu_assert("test_record: not silent while paused", out_buf[i] == 0.0);
-        mu_assert("test_record: changed input while paused", in_buf[i] == test_buf[i]);
+        mu_assert("test_record: changed input while paused", in_buf[i] == test_var);
     }
 
-    looper->settings->requested_state = SlimMessage_Looper_State_PLAY;
-    looper_run(looper, N_FRAMES);
+    instance->settings->requested_state = SlimMessage_Looper_State_PLAY;
+    looper_run(instance, N_FRAMES);
 
     for (int i = 0; i < N_FRAMES; i++)
     {
-        mu_assert("test_record: not playing back recording", out_buf[i] == test_buf[i]);
-        mu_assert("test_record: changed input while playing", in_buf[i] == test_buf[i]);
+        mu_assert("test_record: not playing back recording", out_buf[i] == test_var);
+        mu_assert("test_record: changed input while playing", in_buf[i] == test_var);
     }
 
-    looper_free(looper);
+    looper_free(instance);
 
     return 0;
 }
 
 static char* test_overdub(void)
 {
-    Looper* looper = setup_looper();
-    looper_free(looper);
+    Looper* instance = setup_looper();
+    for (int i = 0; i < N_FRAMES; i++)
+    {
+        in_buf[i]  = 0.1;
+        out_buf[i] = 0.1;
+    }
+    instance->settings->requested_state = SlimMessage_Looper_State_RECORD;
+    looper_run(instance, N_FRAMES);
+    instance->settings->requested_state = SlimMessage_Looper_State_OVERDUB;
+    looper_run(instance, N_FRAMES);
+    for (int i = 0; i < N_FRAMES; i++)
+    {
+        mu_assert("test_overdub: not playing back recording", out_buf[i] == in_buf[i]);
+    }
+    instance->settings->requested_state = SlimMessage_Looper_State_PLAY;
+    looper_run(instance, N_FRAMES);
+    float test_var = 0.2;
+    for (int i = 0; i < N_FRAMES; i++)
+    {
+        mu_assert("test_overdub: not playing back overdub", out_buf[i] == test_var);
+    }
+    looper_free(instance);
+    return 0;
+}
+
+static char* test_insert(void)
+{
+    Looper* instance = setup_looper();
+    float test_buf[N_FRAMES];
+    float test_var;
+    instance->settings->requested_state = SlimMessage_Looper_State_RECORD;
+    for (int i = 0; i < N_FRAMES; i++)
+        in_buf[i]  = 0.1;
+    looper_run(instance, N_FRAMES);
+
+    instance->settings->requested_state = SlimMessage_Looper_State_RECORD;
+    for (int i = 0; i < N_FRAMES; i++)
+        in_buf[i]  = 0.2;
+    looper_run(instance, N_FRAMES);
+
+    instance->settings->requested_state = SlimMessage_Looper_State_PLAY;
+    looper_run(instance, N_FRAMES);
+
+    instance->settings->requested_state = SlimMessage_Looper_State_INSERT;
+    for (int i = 0; i < N_FRAMES; i++)
+        in_buf[i]  = 0.3;
+    looper_run(instance, N_FRAMES);
+
+    for (int i = 0; i < N_FRAMES; i++)
+    {
+        mu_assert("test_insert: not silent while inserting" , out_buf[i] == 0.0);
+    }
+
+    instance->settings->requested_state = SlimMessage_Looper_State_PLAY;
+    looper_run(instance, N_FRAMES);
+
+    test_var = 0.2;
+    for (int i = 0; i < N_FRAMES; i++)
+    {
+        mu_assert("test_insert: not playing back second recording"
+                 , out_buf[i] == test_var
+                 );
+    }
+
+    instance->settings->requested_state = SlimMessage_Looper_State_PLAY;
+    looper_run(instance, N_FRAMES);
+    test_var = 0.1;
+    for (int i = 0; i < N_FRAMES; i++)
+    {
+        mu_assert("test_insert: not playing back first recording", out_buf[i] == test_var);
+    }
+
+    instance->settings->requested_state = SlimMessage_Looper_State_PLAY;
+    looper_run(instance, N_FRAMES);
+    test_var = 0.3;
+    for (int i = 0; i < N_FRAMES; i++)
+    {
+        mu_assert("test_insert: not playing back first recording", out_buf[i] == test_var);
+    }
+    looper_free(instance);
+    return 0;
+}
+
+static char* test_replace(void)
+{
+    Looper* instance = setup_looper();
+    looper_free(instance);
     return 0;
 }
 
 static char* all_tests() 
 {
     mu_run_test(test_record);
+    mu_run_test(test_overdub);
+    mu_run_test(test_insert);
     return 0;
 }
 
